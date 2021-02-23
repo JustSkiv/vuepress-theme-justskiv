9232effae0c71565195f39f58cc606daaae676dd
Nikolay Tuzov <just.skiv@gmail.com>
2021-02-23 16:06:57 +0300

    update package.json and deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/env sh
 
-# 确保脚本抛出遇到的错误
+# Make sure that the script returns error
 set -e
 
-# 生成静态文件
+# Generate static files
 npm run blog:build
 
-# 进入生成的文件夹
+# Go to the generated folder
 cd blog/.vuepress/dist
 
-# 如果是发布到自定义域名
-echo '80shuo.com' > CNAME
+# If it is published to a custom domain name
+echo 'tuzov.su' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
-# 如果发布到 https://<USERNAME>.github.io
+# If posted to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
+# If posted to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:80maker/80maker.github.io.git master:gh-pages
 
 cd -
