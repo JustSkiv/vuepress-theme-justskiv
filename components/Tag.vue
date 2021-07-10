47fab2f7eac9e2a9c2e147d055c2a5cb49558b44
nikolay.tuzov <nikolay.tuzov@lamoda.ru>
2021-07-10 17:02:19 +0300

    little changes

--- components/Tag.vue
+++ components/Tag.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="theme-main__inner theme-tag">
     <div class="theme-tag__list">
-      <h2 class="theme-tag__title">{{$tag.list.length}} tags in total</h2>
-      <router-link class="theme-tag__link" v-for="tag in $tag.list" :key="tag.name" :to="tag.path"># {{ tag.name }}</router-link>
+      <h2 class="theme-tag__title">{{ $tag.list.length }} tags in total</h2>
+      <router-link class="theme-tag__link" v-for="tag in $tag.list" :key="tag.name" :to="tag.path">
+        # {{ tag.name }}
+      </router-link>
     </div>
   </div>
 </template>
@@ -16,10 +18,13 @@ export default {
   background var(--theme-card-background)
   border-radius: 6px;
   padding: 2.15rem;
+
   &__title
+    margin 0 0 1.5rem 0
     margin 0 0 1.5rem 0
     font-size: 1.78571rem;
     font-weight normal
+
   &__link
     display inline-block
     background var(--theme-accent-color-005)
@@ -28,8 +33,8 @@ export default {
     line-height: 2.4;
     transition: background-color .15s;
     margin: 0 1rem 1rem 0;
+
     &:hover
       background var(--theme-accent-color)
       color #ffffff
-  
 </style>
